daa7c10ab3bf2b37bff309f04efae4d863f0bc26
yzlhdy <970716974@qq.com>
2020-12-28 18:03:07 +0800

    123

--- src/components/CardItem/index.tsx
+++ src/components/CardItem/index.tsx
@@ -1,6 +1,8 @@
-import { Box, Grid, Text } from '@chakra-ui/react';
 import React from 'react';
-
+import { Box, Grid, Text } from '@chakra-ui/react';
+import { motion } from 'framer-motion'
+const MotionBox = motion.custom(Box)
+const MotionText = motion.custom(Text)
 interface IProps {
     Lists: any[]
 }
@@ -11,11 +13,19 @@ const CardItem: React.FC<IProps> = ({ Lists }) => {
             <Grid templateColumns={{ md: 'repeat(2, 1fr)', xl: 'repeat(4, 1fr)', lg: 'repeat(3, 1fr)', sm: 'repeat(1, 1fr)' }} gap={3} >
                 {
                     Lists.map((item: any, index: number) => (
-                        <Box w="100" height="200" borderRadius="20px" boxShadow="xl" key={index} bg="green.300" p="20px" m="20px">
+                        <MotionBox w="100" height="200" borderRadius="20px" boxShadow="xl" key={index} bg="green.300" p="20px" m="20px"
+                            initial={{ y: -100, opacity: 0 }}
+                            animate={{ y: 0, opacity: 1 }}
+                            transition={{ type: "spring", delay: 0.5, }}
+                        >
                             <Text overflow="hidden">{item.difficulty}</Text>
-                            <Text overflow="hidden">{item.correct_answer}</Text>
+                            <MotionText overflow="hidden"
+                                initial={{ x: 200, opacity: .2 }}
+                                animate={{ x: 0, opacity: 1 }}
+                                transition={{ type: "keyframes", delay: 0.5 }}
+                            >{item.correct_answer}</MotionText>
                             <Text overflow="hidden">{item.question}</Text>
-                        </Box>
+                        </MotionBox>
                     ))
                 }
             </Grid>
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import { Box, Text } from "@chakra-ui/react"
+import { Box, Button, Flex, ListItem, OrderedList, Text } from "@chakra-ui/react"
 import { motion } from "framer-motion"
 import { NextSeo } from 'next-seo'
 
 // 1. Create a custom motion component from Box
 const MotionBox = motion.custom(Box)
 const MotionText = motion.custom(Text)
+const MotionButton = motion.custom(Button)
+const MotionListItem = motion.custom(ListItem)
+
+
 const About: React.FC = () => {
     const list = { hidden: { opacity: 0 } }
     const item = { hidden: { x: -10, opacity: 0 } }
@@ -14,26 +18,58 @@ const About: React.FC = () => {
         title: "一只灵活的羊",
         description: '一只灵活的养'
     }
+    const childVariants = {
+        hidden: {
+            opacity: 0,
+            x: 200,
+        },
+        visible: {
+            opacity: 1,
+            x: 0,
+            transition: {
+                type: 'spring',
+                delay: 0.5,
+                stiffness: 600,
+                when: 'beforeChildren'
+            }
+        }
+    }
     return (
         <Layout>
             <NextSeo {...seo}></NextSeo>
-            <MotionBox bg="green.500" animate="hidden" w="100px" h="100px" initial={{ scale: 0 }}
-            >
-            </MotionBox>
-            <MotionText bg="puple.200" w="100px" variants={item} pt="30px" whileHover={{ scale: 1.2, rotate: 90 }}
-                whileTap={{
-                    scale: 0.8,
-                    rotate: -90,
-                    borderRadius: "100%"
-                }}>
-                hello world
-             </MotionText>
-            <MotionBox
-                bg="red.200"
-                borderRadius="50%"
+            <Flex justifyContent="space-around" >
+                <MotionBox w="200px" h="200px" bg="green.300" borderRadius="20px"
+                    initial={{ y: 100, opacity: .2 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ type: "spring", delay: .5, stiffness: 500 }}
+                >
+                </MotionBox>
 
-
-            />
+                <MotionBox
+                    initial={{ x: 100, opacity: .2 }}
+                    animate={{ x: 0, opacity: 1 }}
+                    transition={{ type: 'spring', delay: .9 }}
+                >
+                    <MotionButton colorScheme="blue.200" bg="green.300" color="white" px={10} py="4"
+                        whileHover={{
+                            scale: 1.2,
+                            textShadow: 'unset',
+                            boxShadow: "initial",
+                            color: 'pink'
+                        }}
+                    >Submit</MotionButton>
+                </MotionBox>
+            </Flex>
+            <OrderedList pl="30px" mt="30px" lineHeight="50px">
+                <MotionListItem
+                    variants={childVariants}
+                    initial="hidden"
+                    animate="visible"
+                >Lorem ipsum dolor sit amet</MotionListItem>
+                <ListItem>Consectetur1 adipiscing elit</ListItem>
+                <ListItem>Integer molestie lorem at massa</ListItem>
+                <ListItem>Facilisis in pretium nisl aliquet</ListItem>
+            </OrderedList>
         </Layout>
     )
 }
